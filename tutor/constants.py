441780caf69bb0f3c56a7f5178ea79bbdc90d27b
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-07-15 18:04:58 +0600

    class

--- tutor/constants.py
+++ tutor/constants.py
@@ -14,18 +14,18 @@ MEDIUM_OF_INSTRUCTION_CHOICES = [
 ]
 
 CLASS_CHOICES = [
-    ('Class_1', 'Class 1'),
-    ('Class_2', 'Class 2'),
-    ('Class_3', 'Class 3'),
-    ('Class_4', 'Class 4'),
-    ('Class_5', 'Class 5'),
-    ('Class_6', 'Class 6'),
-    ('Class_7', 'Class 7'),
-    ('Class_8', 'Class 8'),
-    ('Class_9', 'Class 9'),
-    ('Class_10', 'Class 10'),
-    ('HSC_1', 'HSC-1st_Year'),
-    ('HSC_2', 'HSC-2nd_Year'),
+    ('Class 1', 'Class 1'),
+    ('Class 2', 'Class 2'),
+    ('Class 3', 'Class 3'),
+    ('Class 4', 'Class 4'),
+    ('Class 5', 'Class 5'),
+    ('Class 6', 'Class 6'),
+    ('Class 7', 'Class 7'),
+    ('Class 8', 'Class 8'),
+    ('Class 9', 'Class 9'),
+    ('Class 10', 'Class 10'),
+    ('HSC 1', 'HSC-1st_Year'),
+    ('HSC 2', 'HSC-2nd_Year'),
 ]
 
 SUBJECT_CHOICES = [
