c0ba3fcbb3fab5dbe487bd636379bb3dfd610fe2
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-08-16 12:07:07 +0600

    2.3

--- tution_media_platform/settings.py
+++ tution_media_platform/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'tution_media_platform.urls'
 
 
 #onrender csrf permission
-CSRF_TRUSTED_ORIGINS = ['https://tution-media-platform.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://tuition-media-platform.onrender.com','https://*.127.0.0.1']
 
 TEMPLATES = [
     {
