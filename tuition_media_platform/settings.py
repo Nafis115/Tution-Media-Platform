b198f92d49bc97779e2265507a7d2062a1a194a2
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-09-30 16:54:49 +0600

    update

--- tuition_media_platform/settings.py
+++ tuition_media_platform/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'tuition_media_platform.urls'
 
 
 #onrender csrf permission
-CSRF_TRUSTED_ORIGINS = ['https://tution-media-platform-backend.onrender.com',"https://tuitionmedia.netlify.app",]
+CSRF_TRUSTED_ORIGINS = ['https://tution-media-platform-backend.vercel.app/',"https://tuitionmedia.netlify.app",]
 
 TEMPLATES = [
     {
